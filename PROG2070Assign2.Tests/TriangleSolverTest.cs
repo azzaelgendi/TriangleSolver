d05b6e7d4f62a0c5b5cfbaf0b37d8ca1b331f77c
Azza Elgendy <37027634+azzaelgendi@users.noreply.github.com>
2019-02-12 11:21:23 -0500

    Contain the completed unit test

--- PROG2070Assign2.Tests/TriangleSolverTest.cs
+++ PROG2070Assign2.Tests/TriangleSolverTest.cs
@@ -15,41 +15,41 @@ namespace PROG2070Assign2.Tests
         public void AnalyzeEquilateralTest()
         {
             TriangleSolver.Analyze(4, 4, 4);
-            Assert.Equals(TriangleSolver.result, "These numbers  form an Equilateral triangle");
+            Assert.AreEqual(TriangleSolver.result, "These numbers  form an Equilateral triangle");
         }
 
         [Test]
         public void AnalyzeIsoscelesTest()
         {
             TriangleSolver.Analyze(4, 4, 5);
-            Assert.Equals(TriangleSolver.result, "These numbers form an Isosceles triangle");
+            Assert.AreEqual(TriangleSolver.result, "These numbers form an Isosceles triangle");
         }
 
         [Test]
         public void AnalyzeNotTriangleTest()
         {
             TriangleSolver.Analyze(50, 4, 5);
-            Assert.Equals(TriangleSolver.result, "These numbers cannot form a triangle");
+            Assert.AreEqual(TriangleSolver.result, "These numbers cannot form a triangle");
         }
 
         [Test]
         public void AnalyzeNegativeVauesTest()
         {
             TriangleSolver.Analyze(0, -4, 5);
-            Assert.Equals(TriangleSolver.result, "These numbers less than zero cannot form a triangle");
+            Assert.AreEqual(TriangleSolver.result, "These numbers less than zero cannot form a triangle");
         }
 
         [Test]
         public void AnalyzeScaleneTest()
         {
             TriangleSolver.Analyze(4, 5, 6);
-            Assert.Equals(TriangleSolver.result, "These numbers  form a Scalene triangle");
+            Assert.AreEqual(TriangleSolver.result, "These numbers  form a Scalene triangle");
         }
         [Test]
         public void AnalyzeZeroTest()
         {
             TriangleSolver.Analyze(0, 0, 6);
-            Assert.Equals(TriangleSolver.result, "These numbers  equal zero cannot form a triangle");
+            Assert.AreEqual(TriangleSolver.result, "These numbers  equal zero cannot form a triangle");
         }
     }
 }
